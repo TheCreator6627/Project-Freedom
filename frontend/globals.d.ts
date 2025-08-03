e0d357025bbd06d68b3f8bb6dd4dd68c87de0ea2
TheCreator6627 <schwertlmike@gmail.com>
2025-08-04 00:06:11 +0200

    101

---
+++ frontend/globals.d.ts
@@ -0,0 +1,4 @@
+// frontend/globals.d.ts
+
+// Diese Zeile erklärt TypeScript, wie es Module behandeln soll, die auf .css enden.
+declare module "*.css";
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@/app/";
+import "@/app/globals.css";
 import { Providers } from "@/context/Web3Provider"; // Dieser Pfad funktioniert dank des Alias weiterhin
 import { form } from "viem/chains";
 

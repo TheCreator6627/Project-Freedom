3db3d0a5d2b3f9e4b869011a705e4ae1626b9ae0
TheCreator6627 <schwertlmike@gmail.com>
2025-08-03 22:17:41 +0200

    101

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
 import { Providers } from "@/context/Web3Provider"; // Dieser Pfad funktioniert dank des Alias weiterhin
 
 const inter = Inter({ subsets: ["latin"] });
